8a482249f4587c2c8dd5029f33cbbd0b89469fde
Jacob <73615055+jacobdev03@users.noreply.github.com>
2023-10-12 15:26:18 +0200

    add form submit listener

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+import fetchWeather from './fetchWeather';
+import displayWeather from './displayWeather';
+const form = document.querySelector('.search');
 
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const cityInput = document.querySelector('.search input');
+  fetchWeather(cityInput.value)
+    .then((data) => displayWeather(data))
+    .catch((err) => console.log(err));
+});
+
+fetchWeather('Warsaw').then((data) => displayWeather(data));
